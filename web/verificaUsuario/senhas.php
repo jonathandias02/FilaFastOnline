501e3faaa91bfd07ca643c87b1e7dff6f4443dd8
Jonathan Almeida Dias <jonathan.a.dias@gmail.com>
2018-10-04 18:10:09 -0300

    Ajax para carregar senhas de atendimento

---
+++ web/verificaUsuario/senhas.php
@@ -0,0 +1,45 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=unieuro;charset=utf8";
+$usuario = "root";
+$senha = "";
+
+try {
+    $conexao = new PDO($dsn, $usuario, $senha);
+} catch (PDOException $e) {
+    echo "conexao_erro";
+    exit;
+}
+
+$filtro = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+$idFila = isset($filtro['idFila']) ? $filtro['idFila'] : null;
+
+$sql = 'SELECT ser.nome, s.id, s.sigla, s.numero, s.identificacao, p.preferencia FROM t_servicos ser, t_senhas s, t_preferencia p
+                WHERE ser.id = s.t_servicos_id AND p.id = s.t_preferencia_id
+                AND s.situacao = "Aguardando" AND s.t_preferencia_id = 1 AND
+                s.t_filas_id = :IDFILA AND s.t_usuario_id IS NULL;';
+
+$stmt = $conexao->prepare($sql);
+$stmt->bindParam(':IDFILA', $idFila);
+$stmt->execute();
+$senhasNormais = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+$sql2 = 'SELECT ser.nome, s.id, s.sigla, s.numero, s.identificacao, p.preferencia FROM t_servicos ser, t_senhas s, t_preferencia p
+                WHERE ser.id = s.t_servicos_id AND p.id = s.t_preferencia_id
+                AND s.situacao = "Aguardando" AND s.t_preferencia_id = 2 AND
+                s.t_filas_id = :IDFILA AND s.t_usuario_id IS NULL;';
+
+$stmt2 = $conexao->prepare($sql2);
+$stmt2->bindParam(':IDFILA', $idFila);
+$stmt2->execute();
+$senhasPreferenciais = $stmt2->fetchAll(PDO::FETCH_OBJ);
+
+if (count($senhasNormais) === 0 && count($senhasPreferenciais) === 0) {
+    echo "NULL";
+} else {
+    if(count($senhasPreferenciais) !== 0){
+        echo json_encode($senhasPreferenciais[0]);
+    }else{
+        echo json_encode($senhasNormais[0]);
+    }    
+}
